0bca82637fd3cbac467df9e26ab8652228c41d87
Prestarocket <prestarocket@gmail.com>
2020-04-09 15:11:34 +0200

    update cache version of ccccss

--- fop_customcss.php
+++ fop_customcss.php
@@ -174,6 +174,11 @@ class Fop_customcss extends Module
         $css_file = dirname(__FILE__).'/views/css/front.css';
 
         file_put_contents($css_file, $compiledCSS);
+
+        //update cache version of ccccss
+        $version = (int) Configuration::get('PS_CCCCSS_VERSION');
+        Configuration::updateValue('PS_CCCCSS_VERSION', ++$version);
+
     }
 
     /**
