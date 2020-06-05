5cdc231b7f6c5c1bfaae70fecc4fee5565a1543c
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2020-06-05 19:05:39 +0200

    Update author info

--- fop_customcss.php
+++ fop_customcss.php
@@ -46,7 +46,7 @@ class Fop_customcss extends Module
         $this->name = 'fop_customcss';
         $this->tab = 'administration';
         $this->version = '1.0.0';
-        $this->author = 'FOP - Guillaume Batier';
+        $this->author = 'Friends of Presta';
         $this->need_instance = 0;
 
         /**
