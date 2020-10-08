2dc2095a29a80543a1126fe99fed646065bf237d
SebSept <sebastienmonterisi@yahoo.fr>
2020-10-08 18:35:08 +0200

    Revert "updated license & added LICENSE.txt"
    This reverts commit 97c551f7

--- fop_customcss.php
+++ fop_customcss.php
@@ -1,20 +1,28 @@
 <?php
+
 /**
- * Copyright since 2020 Friends of Presta
+ * 2007-2020 PrestaShop
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
+ * http://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to <infos@friendsofpresta.org> so we can send you a copy immediately.
+ * to license@prestashop.com so we can send you a copy immediately.
  *
- * @author    Friends of Presta <infos@friendsofpresta.org>
- * @copyright 2020 Friends of Presta
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2020 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
  */
 
 if (!defined('_PS_VERSION_')) {
--- upgrade/upgrade-1.1.0.php
+++ upgrade/upgrade-1.1.0.php
@@ -1,22 +1,18 @@
 <?php
 /**
- * Copyright since 2020 Friends of Presta
+ * 2020-present Friends of Presta community
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to <infos@friendsofpresta.org> so we can send you a copy immediately.
+ * https://opensource.org/licenses/MIT
  *
- * @author    Friends of Presta <infos@friendsofpresta.org>
- * @copyright 2020 Friends of Presta
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @author    Friends of Presta community
+ * @copyright 2020-present Friends of Presta community
+ * @license   https://opensource.org/licenses/MIT MIT
  */
-
 /**
  * /views/css/front.css is under cvs, it should not be.
  * It's shop specific.
